b8013834188d806209448a8df77878b3c6495390
HKST21 <hkst21@proton.me>
2024-12-31 17:20:41 +0100

    created api endpoints succesfull

---
+++ src/api/endpoints.ts
@@ -0,0 +1,96 @@
+import type {
+    Event, Tickets, TicketType, SeatRow, Seat,
+    TestLoginReq, TestLoginRes,
+    OrderReq, OrderRes, User
+} from "./types";
+
+const API_BASE = 'https://nfctron-frontend-seating-case-study-2024.vercel.app';
+
+export const getEvent = async (): Promise<Event> => {
+
+    try {
+
+        const response = await fetch(`${API_BASE}/event`);
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+
+        return response.json();
+    }
+
+    catch (err) {
+        console.error('Error fetching event', err);
+        throw err
+    }
+
+};
+
+export const getEventTix = async (eventId: string): Promise<Tickets> => {
+    try {
+        const response = await fetch(`${API_BASE}/event-tickets?eventId=${eventId}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+
+
+    }
+
+    catch (err) {
+        console.error('Error fetching tix and seatings', err);
+        throw err
+    }
+};
+
+export const testLogin = async (): Promise<TestLoginRes> => {
+    try {
+        const response = await fetch(`${API_BASE}/login`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: "frontend@nfctron.com",
+                password: "Nfctron2025"
+            })
+
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`)
+        }
+
+        return response.json();
+
+    }
+
+    catch (err) {
+        console.error('Error fetching tix and seatings', err);
+        throw err
+    }
+};
+
+export const createOrder = async (order: OrderReq): Promise<OrderRes> => {
+    try {
+        const response = await fetch(`${API_BASE}/order`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+
+        return response.json()
+
+    }
+    catch (err) {
+        console.error('Error fetching tix and seatings', err);
+        throw err
+    }
+};
+
