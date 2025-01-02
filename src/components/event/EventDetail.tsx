5810b049c92804e65638056e04989a005f196159
HKST21 <hkst21@proton.me>
2025-01-02 14:29:56 +0100

    EventDetail done

--- src/components/event/EventDetail.tsx
+++ src/components/event/EventDetail.tsx
@@ -40,10 +40,19 @@ export function EventDetail() {
         <div>
             {loading ? <div>Loading...</div> :
                 <>
-                    <div className="bg-zinc-100 rounded-md h-32" />
+                    <div className="bg-zinc-100 rounded-md h-32" >
+                        <img src={event?.headerImageUrl}
+                            alt={event?.namePub}
+                            className="w-full h-full object-cover rounded-md"
+                        />
+                    </div>
                     <h1 className="text-xl text-zinc-900 font-semibold">{event?.namePub}</h1>
-                    {/* event description */}
                     <p className="text-sm text-zinc-500">{event?.description}</p>
+                    <div className="text-sm text-zinc-500">
+                        <p>Místo: {event?.place}</p>
+                        <p>Datum: {event?.dateFrom ? new Date(event.dateFrom).toLocaleString('cs') : ''} - {event?.dateTo ? new Date(event.dateTo).toLocaleString('cs') : ''}</p>
+                        <p>Měna akce: {event?.currencyIso}</p>
+                    </div>
                 </>}
         </div>
     )
